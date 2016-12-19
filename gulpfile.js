9000a21912bfa1d1160712a9df6c74523869b8e4
javenkn <javenkn7@yahoo.com>
2016-12-18 17:40:30 -1000

    implements browser-sync, gulp, sass, pug

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,25 @@ const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
+const plumber = require('gulp-plumber');
 
 const reload = browserSync.reload;
 
 gulp.task('compile-sass', function () {
-  return gulp.src('./sass/styles.scss', ['compile-sass'])
+  return gulp.src('sass/styles.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 15 versions'],
       cascade: false
     }))
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./public/css'))
+    .pipe(gulp.dest('public/css'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('compile-pug', function () {
   return gulp.src('views/index.pug', ['compile-pug'])
+    .pipe(plumber())
     .pipe(pug())
     .pipe(gulp.dest('./public'));
 });
@@ -34,10 +36,10 @@ gulp.task('browser-sync', ['compile-sass', 'compile-pug'], function () {
 });
 
 gulp.task('watch', ['browser-sync'], function () {
-  gulp.watch('./sass/*', ['compile-sass']);
+  gulp.watch("sass/**/*.scss", ['compile-sass']);
   gulp.watch('./views/*.pug', ['compile-pug']);
-  gulp.watch('./public/css/*.css').on('change', reload);
-  gulp.watch('./public/*.html').on('change', reload);
+  gulp.watch('public/css/*.css', ['compile-pug']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch', 'browser-sync']);
