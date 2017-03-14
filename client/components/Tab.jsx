b007008452b3870606de326fcefcf66304830128
javenkn <javenkn7@yahoo.com>
2017-03-14 00:42:00 -1000

    simplifies code

--- client/components/Tab.jsx
+++ client/components/Tab.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default class Tab extends React.Component {
   render() {
     return (
-      <div className="tab">
-        <h2>{this.props.tabName}</h2>
-      </div>
+      <p className="tab">{this.props.tabName}</p>
     )
   }
 }
