274cf261872773b39d4be0be890c2ba04638297e
javenkn <javenkn7@yahoo.com>
2017-03-14 22:46:30 -1000

    fixes up the code

--- client/components/Tab.jsx
+++ client/components/Tab.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Tab extends React.Component {
   render() {
     return (
-      <p className="tab">{this.props.tabName}</p>
+      <div className="tab">{this.props.tabName}</div>
     )
   }
 }
