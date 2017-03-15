c023d25914f18a794fe9487f14966787714fc7ac
javenkn <javenkn7@yahoo.com>
2017-03-15 00:41:35 -1000

    creates click handler for the navLinks

--- client/components/Detail.jsx
+++ client/components/Detail.jsx
@@ -1,17 +1,45 @@
 import React from 'react';
 import NavLink from './NavLink.jsx';
+import TabBar from './TabBar.jsx';
 require('../sass/detail.scss');
 
 export default class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      numTabsOpen: 0,
+      tabsOpened: []
+    };
+  }
+
+  showTab(tabName, event) {
+    let newTabsOpened = this.state.tabsOpened.slice(); // creates new instance of tabsOpened
+    if(newTabsOpened.indexOf(tabName) == -1) {
+      newTabsOpened.push(tabName);
+    }
+
+    this.setState(prevState => ({
+      numTabsOpen: prevState.numTabsOpen + 1,
+      tabsOpened: newTabsOpened
+    }));
+  }
+
   render() {
+    const tabsOpened = this.state.tabsOpened;
+    const numTabsOpen = this.state.navClicked;
+    const navBarItems = ["About", "Projects", "Contact Me"].map((item, i) => {
+      return (
+        <NavLink key={i} navName={item} showTab={this.showTab.bind(this, item)} />
+      );
+    });
+
     return (
       <div className="detail">
         <div className="navBar">
-          <NavLink navName="About"/>
-          <NavLink navName="Projects"/>
-          <NavLink navName="Contact Me"/>
+          {navBarItems}
         </div>
-        <div className="tabBar" />
+        <TabBar tabsOpened={tabsOpened} numTabsOpen={numTabsOpen}/>
       </div>
     )
   }
