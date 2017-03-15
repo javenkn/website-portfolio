43019915e185ceac876de5dce59aa30aa3d9ebed
javenkn <javenkn7@yahoo.com>
2017-03-15 01:15:51 -1000

    creates open tabs for the tab bar

--- client/components/TabBar.jsx
+++ client/components/TabBar.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Tab from './Tab.jsx';
+require('../sass/tabBar.scss');
 
 export default class TabBar extends React.Component {
 
   render() {
-    console.log(this.props);
+    let tabArr = this.props.tabsOpened;
+    let openTabs = [];
+
+    for(let i = 0; i < tabArr.length; i++) {
+      openTabs.push(<Tab key={i} tabName={tabArr[i]} />);
+    }
 
     return(
       <div className="tabBar">
+        {openTabs}
       </div>
     )
   }
