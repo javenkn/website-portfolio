46237936be99ea6acbb232cc6fa951f6a1e9761d
javenkn <javenkn7@yahoo.com>
2017-03-15 00:43:09 -1000

    adds on click method

--- client/components/NavLink.jsx
+++ client/components/NavLink.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Tab from './Tab.jsx';
 
 export default class NavLink extends React.Component {
   render() {
     return (
-      <div className="navLink">{this.props.navName}
+      <div className="navLink" onClick={this.props.showTab}>
+        {this.props.navName}
       </div>
     );
   }
