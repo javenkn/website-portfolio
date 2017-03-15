c050e225d73b2d4291f61d78b780bfb31f46f534
javenkn <javenkn7@yahoo.com>
2017-03-15 00:41:04 -1000

    initializes tabBar

---
+++ client/components/TabBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tab from './Tab.jsx';
+
+export default class TabBar extends React.Component {
+
+  render() {
+    console.log(this.props);
+
+    return(
+      <div className="tabBar">
+      </div>
+    )
+  }
+}
