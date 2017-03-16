1142925c74ad63551445ef6439b4839d8bc93cb5
javenkn <javenkn7@yahoo.com>
2017-03-15 22:52:04 -1000

    creates onclick method for tabs

--- client/components/Tab.jsx
+++ client/components/Tab.jsx
@@ -5,7 +5,7 @@ require('../sass/exitTab.scss');
 export default class Tab extends React.Component {
   render() {
     return (
-      <div className="tabWrapper">
+      <div className="tabWrapper" onClick={this.props.selectTab}>
         <div className="tab">{this.props.tabName}</div>
         <Exit />
       </div>
