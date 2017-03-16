71f3a2dfd63d086db3da7bf99e15d9d918e27986
javenkn <javenkn7@yahoo.com>
2017-03-15 15:36:49 -1000

    imports the exit tab component and styles

--- client/components/Tab.jsx
+++ client/components/Tab.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Exit from './Exit.jsx';
+require('../sass/exitTab.scss');
 
 export default class Tab extends React.Component {
   render() {
     return (
-      <div className="tab">{this.props.tabName}</div>
-    )
+      <div className="tabWrapper">
+        <div className="tab">{this.props.tabName}</div>
+        <Exit />
+      </div>
+    );
   }
 }
