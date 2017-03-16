dd6e7ad6a2ad227a8af95f4974529e8693aea363
javenkn <javenkn7@yahoo.com>
2017-03-15 15:35:57 -1000

    makes an exit tab div

---
+++ client/components/Exit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Exit extends React.Component {
+  render() {
+    return (
+      <div className='exit'>
+        <span className='exitButton'>&#10005;</span>
+      </div>
+    );
+  }
+}
