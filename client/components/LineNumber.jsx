f0c16a2b04c729459d6812d631bd03d4f375d73c
javenkn <javenkn7@yahoo.com>
2017-03-17 00:44:45 -1000

    makes the class name dynamic

--- client/components/LineNumber.jsx
+++ client/components/LineNumber.jsx
@@ -3,7 +3,7 @@ import React from 'React';
 export default class LineNumber extends React.Component {
   render() {
     return (
-      <div className="line">
+      <div className={this.props.class}>
         {this.props.lineNum}
       </div>
     );
