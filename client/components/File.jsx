68b293062522d1f9815dfefa731917cdbe89c07f
javenkn <javenkn7@yahoo.com>
2017-03-17 00:42:53 -1000

    implements the map function to parse the content and display it using the info component

--- client/components/File.jsx
+++ client/components/File.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Info from './Info.jsx';
 
 export default class File extends React.Component {
   render() {
+
+    let content = this.props.content;
+    console.log(content);
+    let newContent = content.map((element, i) => {
+      return <Info key={i} content={element} lineNum={i} />;
+    });
+
     return (
-      <div className="file">
-        {this.props.content}
+      <div className={this.props.class}>
+        {newContent}
       </div>
     );
   }
