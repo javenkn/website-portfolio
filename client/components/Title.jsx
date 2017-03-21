31c310ee4376b07b24dd3e153ccafa9928e2f324
javenkn <javenkn7@yahoo.com>
2017-03-20 19:49:16 -1000

    implements typed.js plugin and creates readme file

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
 import Tab from './Tab.jsx';
 import LineNumber from './LineNumber.jsx';
-import typeWriter from '../lib/typeWriter.js';
+import '../lib/typed.js';
+import '../sass/readme.scss';
 
 export default class App extends React.Component {
 
   componentDidMount() {
-    let typer = document.getElementById('typewriter');
-    let typewriter = typeWriter.setupTypewriter(typer);
-    typewriter.type();
+    // sets the first line-title's background color
+    document.getElementsByClassName('line-title')[0].style.backgroundColor = '#353C47';
+    Typed.new(".title", { // types out title
+      strings: ["Hi I'm Javen Nakamoto and..."],
+      typeSpeed: 50,
+      callback: () => {
+          setTimeout(() => { // changes the background color to the next line
+            document.getElementsByClassName('typed-cursor')[0].style.display = 'none';
+            document.getElementsByClassName('line-title')[0].style.backgroundColor = '#2B303B';
+            document.getElementsByClassName('line-title')[1].style.backgroundColor = '#353C47';
+
+            Typed.new(".subtitle", { // types out second title
+              strings: ["welcome to my website!"],
+              typeSpeed: 80,
+              callback: () => {
+                setTimeout(() => { // changes the background color to the next line
+                  document.getElementsByClassName('typed-cursor')[1].style.display = 'none';
+                  document.getElementsByClassName('line-title')[1].style.backgroundColor = '#2B303B';
+                  document.getElementsByClassName('line-title')[2].style.backgroundColor = '#353C47';
+
+                  Typed.new(".subtitle2", { // types out third title
+                    strings: ["Click the files on the left to learn more!"],
+                    typeSpeed: 50
+                  });
+                }, 500);
+              }
+            });
+          }, 500);
+        }
+    });
   }
 
   render() {
     return (
-      <div id="typewriter">
-        <LineNumber class="line-title" lineNum={1} />
-        <div className="title">Hi I'm Javen Nakamoto</div>
+      <div className="readmeContainer">
+        <div className="readmeRow">
+          <LineNumber class="line-title" lineNum={1} />
+          <div className="title"></div>
+        </div>
+        <div className="readmeRow">
+          <LineNumber class="line-title" lineNum={2} />
+          <div className="subtitle"></div>
+        </div>
+        <div className="readmeRow">
+          <LineNumber class="line-title" lineNum={3} />
+          <div className="subtitle2"></div>
+        </div>
       </div>
     );
   }
