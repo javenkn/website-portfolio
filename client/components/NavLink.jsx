f7a843af152e99006ed8df9766eaaec63dd598a0
javenkn <javenkn7@yahoo.com>
2017-03-20 22:58:43 -1000

    creates a nav wrapper for the navlinks

--- client/components/NavLink.jsx
+++ client/components/NavLink.jsx
@@ -4,8 +4,10 @@ import Tab from './Tab.jsx';
 export default class NavLink extends React.Component {
   render() {
     return (
-      <div className="navLink" onClick={this.props.showTab}>
-        {this.props.navName}
+      <div className="navLink-wrapper">
+        <div className="navLink" onClick={this.props.showTab}>
+          {this.props.navName}
+        </div>
       </div>
     );
   }
