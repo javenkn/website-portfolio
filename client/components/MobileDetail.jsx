cb77342901f63beaec4b504e56cfbed5d0d4252c
javenkn <javenkn7@yahoo.com>
2017-03-22 17:06:15 -1000

    adds the mobile menu

--- client/components/MobileDetail.jsx
+++ client/components/MobileDetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Footer from './Footer.jsx';
 import MobileNavBar from './MobileNavBar.jsx';
+import MobileMenu from './MobileMenu.jsx';
 import { connect } from 'react-redux';
 import {
   changeTab,
@@ -11,14 +12,16 @@ import '../sass/mobile.scss';
 
 const mapStateToProps = (state) => {
   return state;
-}
+};
 
 class MobileDetail extends React.Component {
   render() {
     return(
       <div className="mobile-detail">
         <MobileNavBar />
+        <MobileMenu />
         <div className="mobile-container">
+          <div className="mobile-title">About</div>
         </div>
         <Footer />
       </div>
