7742cd5a84284ae0dc3b2b2bab30d64297d9afdd
javenkn <javenkn7@yahoo.com>
2017-03-28 21:10:57 +0900

    creates deployment files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile('/index.html');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
---
+++ webpack.production.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './client/app.js', // specifies entry file where bundler starts
+  output: { // location where bundled Javascript code is saved
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+    ]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ]
+};
